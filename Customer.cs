6a1e117025eba15bbceca9fb433605bab11ab780
user <user>
2023-12-11 16:16:44 +0200

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOopHierarchy
+{
+    public class Customer
+    {
+        private  List<Order> _orderHistory = new();
+        public IReadOnlyCollection<Order> OrderHistory => this._orderHistory.AsReadOnly();
+        public string Name { get; set; }
+        public string Email { get; set; }
+       
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        
+        public void AddOrder(Order order)
+        {
+            _orderHistory.Add(order);
+        }
+    }
+}
